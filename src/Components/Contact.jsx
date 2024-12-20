bdea90a2f50a688876b55eb2feb71cf69d3e6cda
Pranitha <pranithanuthalpathi@gmail.com>
2024-12-20 16:59:31 +0530

    Contacts added

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -36,7 +36,24 @@ const ContactSection = styled.section`
   }
 `;
 
+const Icon = styled.a`
+ color: #ddd;
+    text-decoration: none;
+    font-size: 14px;
+    transition: color 0.3s ease;
+
+    &:hover{
+    color: #007bff;}
+`;
+
+const FooterSocial = styled.div`
+display: flex;
+    flex-direction: column;
+    gap: 15px;
+`;
+
 const Contact = () => (
+  <>
   <ContactSection id="contact">
     <h2>Contact Me</h2>
     <form>
@@ -45,7 +62,13 @@ const Contact = () => (
       <textarea placeholder="Your Message" rows="5" />
       <button>Send Message</button>
     </form>
+    <FooterSocial>
+          <Icon href="https://www.instagram.com/harsha_0308/" target="_blank" rel="noopener noreferrer" >Instagram</Icon>
+          {/* <Icon href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</Icon> */}
+          <Icon href="https://www.linkedin.com/in/harsha-vardhan-b1bb8b235/" target="_blank" rel="noopener noreferrer" >LinkedIn</Icon>
+    </FooterSocial>
   </ContactSection>
+</>
 );
 
 export default Contact;
